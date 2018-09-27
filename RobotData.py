db0247244b761b2797c18b0e20f8c24efccf37a0
Jack <sjkai1@126.com>
2018-09-26 21:50:41 -0700

    Add files via upload

--- RobotData.py
+++ RobotData.py
@@ -9,7 +9,7 @@ from torchvision import transforms
 from torch.utils.data.dataset import Dataset  # For custom datasets
 
 class RobotDataset(Dataset):
-    def __init__(self, img_path='/home/data/RobotArm-DA', transform=None):
+    def __init__(self, img_path='/home/data/RobotArm-DA', label_path='/home/data1/full/Group_32_a', transform=None):
         """
         Args:
             img_path (string): path to the folder where images are, /home/data/RobotArm-DA
@@ -19,10 +19,18 @@ class RobotDataset(Dataset):
         # Transforms
         self.transform = transform
         self.img_path = img_path
-        sub_folder = [os.path.join(img_path, o) for o in os.listdir(img_path) 
-                    if os.path.isdir(os.path.join(img_path,o))]
+        self.label_path = label_path
+        sub_folder = sorted([os.path.join(img_path, o) for o in os.listdir(img_path) 
+                    if os.path.isdir(os.path.join(img_path, o))])
+        label_sub_folder = sorted([os.path.join(label_path, o) for o in os.listdir(label_path) 
+                    if os.path.isdir(os.path.join(label_path, o))])
         self.rgb_list = []
         self.depth_list = []
+        self.semantic_label_list = []
+        for sub in label_sub_folder:
+            sub_foler = os.path.join(sub, 'RGB')
+            self.semantic_label_list.extend([os.path.join(sub_foler, semantic_label_folder) for semantic_label_folder in sorted(os.listdir(sub_foler)) if os.path.isdir(semantic_label_folder)])
+            
         for sub in sub_folder:
             rgb_sub_foler = os.path.join(sub, 'RGB')
             self.rgb_list.extend([os.path.join(rgb_sub_foler, img_file) for img_file in sorted(os.listdir(rgb_sub_foler)) if img_file.split('.')[-1] == 'jpg'])
@@ -30,6 +38,7 @@ class RobotDataset(Dataset):
             self.depth_list.extend([os.path.join(depth_sub_foler, depth_file) for depth_file in sorted(os.listdir(depth_sub_foler)) if depth_file.split('.')[-1] == 'npy'])
         print('Number of RGB images: ', len(self.rgb_list))
         print('Number of Depth images: ', len(self.depth_list))
+        print('Number of Label images: ', len(self.semantic_label_list))
 
         assert len(self.rgb_list) == len(self.depth_list), 'RGB != Depth'
         self.data_len = len(self.rgb_list)
@@ -37,16 +46,18 @@ class RobotDataset(Dataset):
     def __getitem__(self, index):
         rgb_name = self.rgb_list[index]
         depth_name = self.depth_list[index]
+        semantic_label_name = os.path.join(self.semantic_label[index], 'direct_mask.png')
         print('rgb_name: ', rgb_name, 'depth_name: ', depth_name)
         # Open image
         rgb_img = Image.open(rgb_name)
+        semantic_label = Image.open(semantic_label_name)
         depth_arr = np.load(depth_name) / 1000. # millimeter -> meter
 
         # If there is an operation
         if self.transform:
             rgb_img = self.transform(rgb_img)
 
-        return rgb_img, depth_arr
+        return rgb_img, depth_arr, semantic_label
 
     def __len__(self):
         return self.data_len
@@ -55,6 +66,7 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     custom_RobotDataset =  RobotDataset(img_path='/home/data/RobotArm-DA',
+                                        label_path='/home/data1/full/Group_32_a',
                                         transform=transforms.Compose([
                                                 transforms.Resize([128, 128]),
                                                 transforms.ToTensor(),
